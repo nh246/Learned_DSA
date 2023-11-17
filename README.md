7085fa79f3525030be3a1786d2329dbf3f57582e
nh246 <nazmul210130@diit.edu.bd>
2023-11-17 21:02:44 +0600

    adding code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Learned_DSA
+
--- Setup-Cpp/code.cpp
+++ Setup-Cpp/code.cpp
@@ -1,9 +1,35 @@
 #include<iostream>
+
+#include<bits/stdc++.h>
+
 using namespace std;
 
+ 
+
 int main() {
-int x ;
-cin >> x;
-    cout << "Value of x: " << x ;
-    return 0;
+
+    // Write your code here
+
+    char ch;
+
+    cin>>ch;
+
+ 
+
+    if (isupper(ch))
+
+        cout<<1;
+
+    else if(islower(ch))
+
+        cout<<0;
+
+    else
+
+        cout<<-1;
+
+    
+
+    return 0;   
+
 }
